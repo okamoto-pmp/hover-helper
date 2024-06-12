a5b14544999aefd1b1fa7ebfb05805116ff42667
okamoto-pmp <okamoto-k@dentsu-pmp.co.jp>
2024-06-12 11:01:41 +0900

    feat(manifest.json): add permissions, icons, and background configuration for improved extension functionality feat(package.json): add "@types/chrome" dependency for better TypeScript support feat(src/App.tsx): add button to enqueue action and handleEnqueue function to send message to background script feat(src/background/index.ts): create background script to handle enqueue action and show notification with timestamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
 
 function App() {
+  const handleEnqueue = () => {
+    chrome.runtime.sendMessage({
+      action: "enqueue",
+    });
+  };
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button
+        onClick={handleEnqueue}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Enqueue
+      </button>
     </>
   );
 }
---
+++ src/background/index.ts
@@ -0,0 +1,16 @@
+chrome.runtime.onMessage.addListener(async (request) => {
+    if (request.action === "enqueue") {
+        await queue();
+    }
+});
+
+
+const queue = async () => {
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: chrome.runtime.getURL("icon48.jpg"),
+        title: "Hello World",
+        message: `Enqueued at ${new Date().toLocaleTimeString()}`,
+        priority: 2,
+    });
+};
